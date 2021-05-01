0a309775ae5d9499ce51c624866888f6775e0ad4
Touhid <touhid4bd@gmail.com>
2021-05-02 01:29:54 +0600

    UI Card and Expense Items added

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,18 @@
+import Card from "../UI/Card";
+import ExpenseItem from "./ExpenseItem";
+import './Expenses.css';
+
+const Expenses = (props) => {
+    
+      return <Card className="expenses">
+        {props.items.map(expense => (
+          <ExpenseItem 
+            key = {expense.id}
+            expenseDate = {expense.date}
+            expenseTitle = {expense.title}
+            expenseAmount = {expense.amount} 
+          />
+        ))}
+      </Card>
+}
+export default Expenses;
